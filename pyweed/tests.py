37fa25511ffe723015f90d3f8cf1cf15576139fa
Lukasz Boldys <mail@utek.pl>
2014-04-25 08:59:05 +0200

    code cleanup, version bump

--- pyweed/tests.py
+++ pyweed/tests.py
@@ -1,13 +1,13 @@
 import unittest
 
-from . import weed
+from . import utils
 
 
 def _mock_request(*args, **kwargs):
     return {"status": "200"}, b"OK"
 
 
-weed.http.request = _mock_request
+utils.http.request = _mock_request
 
 
 class MiscTests(unittest.TestCase):
@@ -16,17 +16,17 @@ class MiscTests(unittest.TestCase):
         pass
 
     def test_boundary(self):
-        bound = weed._generate_boundary()
+        bound = utils._generate_boundary()
         self.assertIsNotNone(bound)
 
     def test_get_data(self):
-        content = weed._get_data("url")
+        content = utils._get_data("url")
         self.assertEqual(content, "OK")
 
     def test_post_data(self):
-        content = weed._post_data("url", "123")
+        content = utils._post_data("url", "123")
         self.assertEqual(content, "OK")
 
     def test_delete_data(self):
-        content = weed._delete_data("url")
+        content = utils._delete_data("url")
         self.assertTrue(content)
