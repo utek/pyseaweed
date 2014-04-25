969c3dd01c79cdae8d9c0b9b9dc53087cc339502
Lukasz Boldys <mail@utek.pl>
2014-04-25 12:38:33 +0200

    fixed tests

--- pyweed/func_tests.py
+++ pyweed/func_tests.py
@@ -1,7 +1,6 @@
 from __future__ import print_function
 import os
 import unittest
-from unittest.case import SkipTest
 from .weed import WeedFS
 
 
@@ -11,10 +10,7 @@ class FunctionalTests(unittest.TestCase):
         self.weed = WeedFS()
 
     def test_upload_delete(self):
-        try:
-            fid = self.weed.upload_file(__file__)
-            self.assertIsNotNone(fid)
-            res = self.weed.delete_file(fid)
-            self.assertTrue(res)
-        except:
-            raise SkipTest
+        fid = self.weed.upload_file(__file__)
+        self.assertIsNotNone(fid)
+        res = self.weed.delete_file(fid)
+        self.assertTrue(res)
